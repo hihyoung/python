db76d9db9f20cb47bcad976523b30c92d7151c2b
hihyoung <36256162+hihyoung@users.noreply.github.com>
2018-02-08 16:59:01 +0900

    drawing

---
+++ test0129.py
@@ -0,0 +1,17 @@
+a=int(input("첫 번째 숫자를 입력하세요 : "))
+b=int(input("두 번째 숫자를 입력하세요 : "))
+print(a)
+print(b)
+print(100)
+print("a")
+print(a+b)
+print(a, "+", b, "=", a+b)
+result=a+b
+print(a, "+", b, "=", result)
+result=a-b
+print(a, "-", b, "=", result)
+result=a*b
+print(a, "*", b, "=", result)
+result=a/b
+print(a, "/", b, "=", result)
+
